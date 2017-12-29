ebeacd3df167e2712e7fc2c32bc63ae4c26b9063
Tamara <tamara.czakert@students.fh-hagenberg.at>
2017-12-29 17:03:37 +0100

    17:03

--- index.js
+++ index.js
@@ -11,9 +11,36 @@ server.use(bodyParser.json());
 
 server.post('/get-cocktail', function (req, res) {
 
-
     if (req.body.result.action == "/get-cocktail-by-ingredient") {
 
+        let ingredientToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.Ingredient ? req.body.result.parameters.Ingredient : 'Vodka';
+        let reqUrl = encodeURI('http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredientToSearch);
+        http.get(reqUrl, (responseFromAPI) => {
+            let body = '';
+            responseFromAPI.on('data', function (chunk) {
+                body += chunk;
+            });
+
+            responseFromAPI.on('end', function () {
+                let cocktail = JSON.parse(body)['drinks'][0];
+                let dataToSend = ingredientToSearch === 'Vodka' ? 'I don\'t have the required info on that. Here\'s some info on \'Vodka\' instead.\n ' : '';
+                dataToSend += cocktail['strDrink'];
+
+                return res.json({
+                    speech: dataToSend,
+                    displayText: dataToSend,
+                    source: 'get-cocktail-by-ingredient'
+                });
+
+            });
+        }, (error) => {
+            return res.json({
+                speech: 'Something went wrong!',
+                displayText: 'Something went wrong!',
+                source: 'get-cocktail-by-ingredient'
+            });
+        });
+    } else {
         let ingredientToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.Ingredient ? req.body.result.parameters.Ingredient : 'Vodka';
         let reqUrl = encodeURI('http://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredientToSearch);
         http.get(reqUrl, (responseFromAPI) => {
